17275a5c2a14c19fd417ea915780f0d7b978610b
Tomas Langebaek <t.langebaek@uniandes.edu.co>
2021-05-18 21:55:42 -0500

    barra

--- bono-d3/src/App.js
+++ bono-d3/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { drawChart } from './components/bar';
+import * as d3 from 'd3';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    d3.json(
+      'https://gist.githubusercontent.com/josejbocanegra/000e838b77c6ec8e5d5792229c1cdbd0/raw/83cd9161e28e308ef8c5363e217bad2b6166f21a/countries.json'
+    ).then((data) => {
+      console.log(data);
+      drawChart(data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Purchasing Power vs Life Espectancy</h2>
+      <div id='chart'></div>
     </div>
   );
 }
---
+++ bono-d3/src/components/bar.js
@@ -0,0 +1,45 @@
+import * as d3 from 'd3';
+
+export function drawChart(data) {
+  const canvas = d3.select('#chart');
+  const width = 700;
+  const height = 500;
+  const margin = { top: 10, left: 50, bottom: 40, right: 10 };
+  const iwidth = width - margin.left - margin.right;
+  const iheight = height - margin.top - margin.bottom;
+
+  const svg = canvas.append('svg');
+  svg.attr('width', width);
+  svg.attr('height', height);
+
+  let g = svg
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+  const y = d3.scaleLinear().domain([0, 100]).range([iheight, 0]);
+
+  const x = d3
+    .scaleBand()
+    .domain(data.map((d) => d.purchasingpower))
+    .range([0, iwidth])
+    .padding(0.1);
+
+  const bars = g.selectAll('rect').data(data);
+
+  bars
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .style('fill', 'steelblue')
+    .attr('x', (d) => x(d.purchasingpower))
+    .attr('y', (d) => y(d.lifeexpectancy))
+    .attr('height', (d) => iheight - y(d.lifeexpectancy))
+    .attr('width', x.bandwidth());
+
+  g.append('g')
+    .classed('x--axis', true)
+    .call(d3.axisBottom(x))
+    .attr('transform', `translate(0, ${iheight})`);
+
+  g.append('g').classed('y--axis', true).call(d3.axisLeft(y));
+}
